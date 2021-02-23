ca48c7bbaf585434f680a78771e84c3a5fca068b
Mazharul-Haque <mshagor009@gmail.com>
2021-02-24 00:32:01 +0600

    dynamic  data load

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   const nayoks = ['Anwar', 'joshim', 'Maruf']
+  const products = [{name : 'photoshop', price : '$99.35'}]
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a reader</p>
+        <Product name={products[0].name}  ></Product>
+        
         <Person name = "Munna" Profession ="Jobless"  ></Person>
         <Person name = "Sadik" Profession ="Football"  ></Person>
         <Person></Person>
@@ -18,7 +21,23 @@ function App() {
     </div>
   );
 }
-
+function Product(props){
+  const productStyle={
+    border : '1px solid yellow',
+    borderRadius : '5px',
+    backgroundColor : 'lightgray',
+    height : '200px',
+    width : '200px',
+    float : 'left'
+  }
+  return (
+    <div style={productStyle} >
+        <h2>{props.name}</h2>
+        <h1></h1>
+        <button>Buy Now</button>
+    </div>
+  )
+}
 function Person(props){
   return (
     <div>
